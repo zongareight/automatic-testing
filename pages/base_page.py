0ddc9d0ef97ab19796982d17e11917a84b4b2e7a
zongareight <zongareight@gmail.com>
2019-06-28 00:11:35 +0300

    added new class LoginPage and Locators

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
 
 
-class BasePage(object):
+class BasePage:
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,14 +1,13 @@
+from .locators import MainPageLocators
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
 
     def should_be_login_link(self):
         assert self.is_element_present(
-            By.CSS_SELECTOR,
-            "#login_link"
+            *MainPageLocators.LOGIN_LINK
         ), 'Login link is not presented'
