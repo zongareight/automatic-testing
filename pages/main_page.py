8579ff26b7aeccbba1c0fd4ba5cb4ce619a1edfe
zongareight <zongareight@gmail.com>
2019-06-27 16:51:17 +0300

    update file, added method is_element_present

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,9 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            By.CSS_SELECTOR,
+            "#login_link"
+        ), 'Login link is not presented'
